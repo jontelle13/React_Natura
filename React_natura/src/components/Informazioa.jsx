dd3465ad783dff5be158bb371580fa27160a744d
Jontelle <jtelleriaga22wg@ikzubirimanteo.com>
2024-01-14 20:25:27 +0100

    bukatuta

--- React_natura/src/components/Informazioa.jsx
+++ React_natura/src/components/Informazioa.jsx
@@ -1,13 +1,58 @@
+import React from "react";
 
-export default function Informazioa() {
-
-    return (
-      <>
-      <div className="border">
-        <h1>Informazioa</h1>
-      </div>
-      </>
-    )
-  }
-  
-  
+export default function Informazioa({ selectedEspacio }) {
+  return (
+    <div className="border">
+      <h1>Informazioa</h1>
+      {selectedEspacio ? (
+        <form>
+          <p>
+            Izena
+            <input
+              type="text"
+              name="izena"
+              value={selectedEspacio.name}
+              readOnly
+            />
+            Tokia
+            <input
+              type="text"
+              name="tokia"
+              value={selectedEspacio.location}
+              readOnly
+            />
+          </p>
+          <p>
+            Mota
+            <input
+              type="text"
+              name="mota"
+              value={selectedEspacio.type}
+              readOnly
+            />
+          </p>
+          <p>
+            Turismo euskadi
+            <input
+              type="text"
+              name="turismo"
+              value={selectedEspacio.turismoEuskadi}
+              readOnly
+            />
+          </p>
+          <p>
+            Deskripzioa
+            <input
+              type="text"
+              name="deskripzioa"
+              value={selectedEspacio.description}
+              readOnly
+            />
+          </p>
+        </form>
+      ) : (
+        <p>No space selected</p>
+      )}
+    </div>
+  );
+}
--- React_natura/src/components/Udalerria.jsx
+++ React_natura/src/components/Udalerria.jsx
@@ -1,31 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import Informazioa from "./Informazioa";
 
 const Udalerria = ({ selectedProbintzia }) => {
-  const [udalerriak, setUdalerriak] = useState([]);
+  const [territories, setTerritories] = useState([]);
+  const [selectedUdalerria, setSelectedUdalerria] = useState("");
+  const [espaciosNaturales, setEspaciosNaturales] = useState([]);
+  const [selectedEspacio, setSelectedEspacio] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://opendata.euskadi.eus/contenidos/ds_recursos_turisticos/espacios_naturales_euskadi/opendata/espacios-naturales.json"
-        );
-
-        if (!response.ok) {
-          throw new Error("Error fetching data");
-        }
-
-        const data = await response.json();
-        const parkeak = data.espacios_naturales;
-
-        const udalerriakArray = parkeak.map((parkea) => parkea.territory);
-        setUdalerriak(udalerriakArray);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
+      const response = await fetch('/espacios-naturales.json');
+      const data = await response.json();
+      setTerritories(data);
     };
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (selectedUdalerria) {
+      const udalerriaAukera = territories.find(
+        (territory) => territory.name === selectedUdalerria
+      );
+
+      if (udalerriaAukera) {
+        setEspaciosNaturales(udalerriaAukera.espaciosNaturales || []);
+      }
+    } else {
+      setEspaciosNaturales([]);
+    }
+  }, [selectedUdalerria, territories]);
+
+  const udalerriAldatu = (event) => {
+    setSelectedUdalerria(event.target.value);
+  };
+
+  const espacioAukeratu = (event) => {
+    const selectedEspacioName = event.target.value;
+    const selectedEspacio = espaciosNaturales.find(
+      (espacio) => espacio.name === selectedEspacioName
+    );
+
+    setSelectedEspacio(selectedEspacio);
+  };
 
   return (
     <div className="border">
@@ -33,13 +50,20 @@ const Udalerria = ({ selectedProbintzia }) => {
       {selectedProbintzia && (
         <div>
           <p>{selectedProbintzia} Probintzian dauden udalerriak:</p>
-          <select>
-            {udalerriak.map((udalerri, index) => (
-              <option key={index}>{udalerri}</option>
+          <select value={selectedUdalerria} onChange={udalerriAldatu}>
+            {territories.map((territory) => (
+              <option key={territory.id}>{territory.name}</option>
+            ))}
+          </select>
+          <p> Parke naturalak:</p>
+          <select onChange={espacioAukeratu}>
+            {espaciosNaturales.map((espacio) => (
+              <option key={espacio.id}>{espacio.name}</option>
             ))}
           </select>
         </div>
       )}
+      <Informazioa selectedEspacio={selectedEspacio} />
     </div>
   );
 };
